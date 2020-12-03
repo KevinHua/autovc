e20571c345555c32ed6b6cfd3dc623288e48b046
Hua Hongliang <huahl@126.com>
2020-12-03 16:45:40 +0800

    some fix

--- split_wav.py
+++ split_wav.py
@@ -28,13 +28,17 @@ def split_wav_by_speaker(audio_path, output_root, sampling_rate, segment_seconds
     os.makedirs(output_dir, exist_ok=True)
     
     fl = sorted(os.listdir(audio_path));
+    seg_index = 1
     for ft in fl:
         fn = os.path.join(audio_path, ft)
         if os.path.isfile(fn):            
             y, sr = librosa.load(fn, sampling_rate)
+            if len(y) == 0:
+                print('{} load failed.'.format(fn))
+                continue
+            
             segment_len = math.ceil(segment_seconds * sampling_rate)
             pos = 0
-            seg_index = 1
             while pos < len(y):
                 seg = y[pos:pos + segment_len]
                 
@@ -49,9 +53,9 @@ def parse_args():
     parser.add_argument("--audio_path", type=str, help="A dirctory that contains all voice audios of a speaker when multi_speakers is 0. Or a containing folder of multiple speaker subfolders")
     parser.add_argument("--output_root", type=str, help="path to save splitted result waves")
     parser.add_argument("--sampling_rate", type=int, default=16000, required=False, help="the output wave's sampling rate, 16000 by default")
-    parser.add_argument("--segment_seconds", type=int, default=5, required=False, help="duration in seconds of splitted wave segment")
-    parser.add_argument("--multi_speakers", type=int, default=1, help="multiple speakers mode or one speaker mode")
-    parser.add_argument("--speaker_id",  type=int, default=None, required=False, help="speaker id to make a directory that contains all splitted waves. valid only when multi_speakers equals 0")
+    parser.add_argument("--segment_seconds", type=int, default=5, required=False, help="duration in seconds of splitted wave segment. 5 seconds by default")
+    parser.add_argument("--multi_speakers", type=int, default=1, help="1 - multiple speakers mode or 0 - one speaker mode. multi-mode by default")
+    parser.add_argument("--speaker_id",  type=int, default=None, required=False, help="speaker id to make a directory that contains all splitted waves. valid only when multi_speakers equals 0. None by default")
     config = parser.parse_args()
     print(config)
     return config
